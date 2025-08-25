7630556f90fb405feff842dcbfa50c07e957b659
Harry (Haochen Wang) <harrywang0619@gmail.com>
2025-08-25 11:04:49 -0400

    Flavour Tagging features (#3)
    * Flavour Tagging features
    
    Flavour Tagging features. (src/, examples/flavour-tagging)
    
    * run Formatter
    
    * try to fix github action
    
    * Try to make a temp alias of EEJet as the released version of JetReconstruction still have EEjet
    
    * update some documentation
    
    * Fixups for initial version
    
    A few improvements to .gitignore
    Make dependency on JetReconstruction for v1.0 (currently in development)
    Change back to EEJet (no alias needed)
    Update calling signatures
    
    * Update README
    
    * Make the example work
    
    Changed EEJet type alias to EEJet from JetReconstruction
    Using Pkg in the example to compile 1.0.0-DEV version of JetReconstruction
    
    * Run JuliaFormatter
    
    * More optimizations and clean up unused helper docs.
    
    More sincos() and inline simd to optimize.
    
    ---------
    
    Co-authored-by: Graeme A Stewart <graeme.andrew.stewart@cern.ch>

--- src/JetTaggingFCC.jl
+++ src/JetTaggingFCC.jl
@@ -1,5 +1,36 @@
 module JetTaggingFCC
 
-# Write your package code here.
+using JetReconstruction
+using JetReconstruction: EEJet
+using EDM4hep
+using JSON
+using ONNXRunTime
+using StructArrays: StructVector
+using LorentzVectorHEP
 
-end
+const JetConstituents = StructVector{ReconstructedParticle,<:Any}
+const JetConstituentsData = Vector{Float32}
+
+# Include constituent utilities (as a module for now)
+include("JetConstituentUtils.jl")
+using .JetConstituentUtils
+
+# Include jet constituent builder functions
+include("JetConstituentBuilder.jl")
+
+# Include flavour helper functions
+include("JetFlavourHelper.jl")
+
+# Export all public functions
+export build_constituents_cluster
+export extract_features
+export setup_onnx_runtime
+export prepare_input_tensor
+export get_weights
+export get_weight
+
+
+# Export types
+export JetConstituents, JetConstituentsData
+
+end # module JetTaggingFCC
